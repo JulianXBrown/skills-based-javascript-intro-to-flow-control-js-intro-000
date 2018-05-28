440b7c1f3e464c0e16e35001d183131fa5d4177a
Julian Brown <julianxbrown@gmail.com>
2018-05-28 20:49:22 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -2,10 +2,12 @@ function basicTeenager(age) {
 if (age >= 13 && age <= 19){
   return "You are a teenager!";
 }
+  
 }
-
 function teenager(age) {
-
+if (age >= 13 && age <= 19){
+  return "You are a teenager!"
+} else learn 
 }
 
 function ageChecker(age) {
